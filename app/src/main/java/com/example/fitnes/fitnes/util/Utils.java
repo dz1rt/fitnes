9423cfea9fb107a7ce63b900846669297a754e0d
afftarkapkov <afftarkapkov@mail.ru>
2018-10-20 23:32:16 +0300

    utils

---
+++ app/src/main/java/com/example/fitnes/fitnes/util/Utils.java
@@ -0,0 +1,44 @@
+package com.example.fitnes.fitnes.util;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Utils {
+    public static void hideKeyboard(@NonNull Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = activity.getCurrentFocus();
+        if (view == null)
+            view = new View(activity);
+        if (imm != null)
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static String calculateMD5(@NonNull String source) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.reset();
+            byte[] resultByte = md.digest(source.getBytes(Charset.forName("UTF8")));
+            return Hex.encodeHexString(resultByte);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void toast(Activity activity, String message) {
+        activity.runOnUiThread(() -> Toast.makeText(activity, message, Toast.LENGTH_SHORT).show());
+    }
+
+    public static void toast(Activity activity, String message, int duration) {
+        activity.runOnUiThread(() -> Toast.makeText(activity, message, duration).show());
+    }
+}
